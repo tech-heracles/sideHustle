299e176f206172b858eaa1885f92d7dbb009a176
henrik_balla <henrik.balla@imb.al>
2023-01-19 00:09:34 +0100

    fix

--- PlatinumWeb/FtoPerdorues.aspx.cs
+++ PlatinumWeb/FtoPerdorues.aspx.cs
@@ -74,7 +74,7 @@ JwhY6kCDqCIFqgK6rktEaOMCAwEAAQ==
                 plaintext = ByteConverter.GetBytes(JsonConvert.SerializeObject(json));
                 encryptedtext = clsFunksione.encrypt(plaintext, RSA.ExportParameters(false), false);
                 string base64 = Convert.ToBase64String(encryptedtext);
-                clsFunksione.gjeneroLinkPerKonfirmimEmaili("henrik.balla@imb.al", base64);
+                clsFunksione.gjeneroLinkPerKonfirmimEmaili(email_inline.Text, base64);
                 email_inline.Text = "";
             }
             catch(Exception ex)
