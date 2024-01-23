1903f738e7facb7a8d4d3c191c473e59b2252310
KhalRico <Henrik_Su@bitbucket.org>
2024-01-23 13:59:57 +0100

    fixes

--- DbCore/Utility/Logging.cs
+++ DbCore/Utility/Logging.cs
@@ -9,28 +9,28 @@ using Newtonsoft.Json;
 
 namespace DbCore.Utility
 {
-    public static class Logging
-    {
-        private const string _logUrl = "https://europe-west1-alphaweb.cloudfunctions.net/logAlphawebView";
-        public static async Task<bool> Log(BigQueryLogMessage message)
-        {
-            try
-            {
-                string componentName = clsKomponente.merrKomponenteLikeSipasEmrit(message.view.Split('/')[1], true);
-                message.view = componentName;
-                if (message.view.IsNullOrEmpty()) return true;
-                using (HttpClient client = new HttpClient())
-                {
-                    string json = JsonConvert.SerializeObject(message);
-                    StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
-                    await client.PostAsync(_logUrl, data);
-                }
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-    }
+	public static class Logging
+	{
+		private const string _logUrl = "https://europe-west1-alphaweb.cloudfunctions.net/logAlphawebView";
+		public static async Task<bool> Log(BigQueryLogMessage message)
+		{
+			try
+			{
+				string componentName = clsKomponente.merrKomponenteLikeSipasEmrit(message.view.Split('/')[1], true);
+				message.view = componentName;
+				if (message.view.IsNullOrEmpty()) return true;
+				using (HttpClient client = new HttpClient())
+				{
+					string json = JsonConvert.SerializeObject(message);
+					StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+					await client.PostAsync(_logUrl, data);
+				}
+				return true;
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+	}
 }
