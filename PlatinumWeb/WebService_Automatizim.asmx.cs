6f940ba1de57dd0ad8641acd326d09a04d933b72
KhalRico <Henrik_Su@bitbucket.org>
2024-04-29 12:20:12 +0200

    fix

--- PlatinumWeb/WebService_Automatizim.asmx.cs
+++ PlatinumWeb/WebService_Automatizim.asmx.cs
@@ -1238,7 +1238,7 @@ namespace PlatinumWeb
                             !konfigurimAmbjenti.KodKonfigAmbjente.Contains("USHmag"), ref dbData, krijoartri, "", "", false, out mesazhmevonshem, false, String.IsNullOrEmpty(nrdok), iic, nivf);
                 if (msg.PershkrimMesazhi.Contains("Sasia e daljes është më e madhe se gjendja e artikullit"))
                 {
-                    gjeneroDokMag = false;
+                    gjeneroDokMag = true;
                     koka.IdStatusDok = 0;
                     idMenyrePAgese = 8;
                     clsMesazh draftMessage = koka.krijoShitje(ref gjeneroDokMag, konfigurimAmbjenti.IdNivel, 0, konfigurimAmbjenti.IdKonfigAmbjente, kf.IdKlientFurnitor, kf.KodKlientFurnitor, 0, "0", dtDok, nrdok, nrSerial, dtDok, monedha.IdMonedha, monedha.KodiMonedha,
